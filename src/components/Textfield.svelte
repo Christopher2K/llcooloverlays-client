97d00105561522eb227745623927298defd33692
Christopher N. KATOYI KABA <me@christopher2k.dev>
2023-02-05 18:03:15 -0500

    fix: disable autocapitalize

--- src/components/Textfield.svelte
+++ src/components/Textfield.svelte
@@ -7,6 +7,7 @@
 <fieldset class="w-full mb-5">
   <label class="inline-block w-full text-l font-medium mb-3" for={name}>{label}</label>
   <input
+    autocapitalize="false"
     class="w-80 px-2 py-2 rounded-md border border-neutral-400"
     {name}
     type="text"
